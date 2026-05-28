1cd93ea27dc1de7c96630075689888fc99c54dd7
carsmerge-lab <carsmerge@gmail.com>
2026-05-28 18:23:34 -0400

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,19 @@
 export const firebaseConfig = {
-  apiKey: "PASTE_FIREBASE_API_KEY_HERE",
-  authDomain: "PASTE_FIREBASE_AUTH_DOMAIN_HERE",
-  databaseURL: "PASTE_FIREBASE_DATABASE_URL_HERE",
-  projectId: "PASTE_FIREBASE_PROJECT_ID_HERE",
-  storageBucket: "PASTE_FIREBASE_STORAGE_BUCKET_HERE",
-  messagingSenderId: "PASTE_FIREBASE_MESSAGING_SENDER_ID_HERE",
-  appId: "PASTE_FIREBASE_APP_ID_HERE"
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDiGCGaEUDSkZRM4Wvko4a45TsJkr-Kq7I",
+  authDomain: "haydo-lie-detector.firebaseapp.com",
+  databaseURL: "https://haydo-lie-detector-default-rtdb.firebaseio.com",
+  projectId: "haydo-lie-detector",
+  storageBucket: "haydo-lie-detector.firebasestorage.app",
+  messagingSenderId: "470148443985",
+  appId: "1:470148443985:web:3b1303ea12ac7646b36d70"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
